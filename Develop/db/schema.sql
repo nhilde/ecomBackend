40613de4f650def8a12b8e8db2f07b563385c24d
Noah Hildebrand <nhilde21@gmail.com>
2022-05-03 00:31:17 -0400

    worked on routes

--- Develop/db/schema.sql
+++ Develop/db/schema.sql
@@ -4,3 +4,30 @@ DROP DATABASE IF EXISTS ecommerce_db;
 -- CREATE DATABASE
 CREATE DATABASE ecommerce_db;
 
+CREATE TABLE Category {
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    category_name VARCHAR(30) NOT NULL
+},
+
+CREATE TABLE Product {
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    product_name VARCHAR(30) NOT NULL,
+    price DEC(3,2) NOT NULL,
+    stock INT NOT NULL DEFAULT 10,
+    FOREIGN KEY (category_id) REFERENCES Category(id),
+    CHECK (price = DEC AND stock = INT)
+}
+
+CREATE TABLE Tag {
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    tag_name VARCHAR(30)
+}
+
+CREATE TABLE ProductTag {
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    FOREIGN KEY (product_id) REFERENCES Product(id),
+    FOREIGN KEY (tag_id) REFERENCES Tag(id)
+}
+
+
+
--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -5,24 +5,74 @@ const { Category, Product } = require('../../models');
 
 router.get('/', (req, res) => {
   // find all categories
-  // be sure to include its associated Products
+  try {
+    const allCategories = await Category.findAll({
+      include: [
+        {
+          model: Product,
+          attributes: ['id', 'product_name', 'price', 'stock', 'category_id'],
+        },
+      ],
+    });
+    res.json(allCategories);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
+// be sure to include its associated Products
+
 
 router.get('/:id', (req, res) => {
-  // find one category by its `id` value
-  // be sure to include its associated Products
+  try {
+    const categoryData = await Category.findByPk(req.params.id, {
+      include: [
+        {
+          model: Product,
+          attributes: ['id', 'product_name', 'price', 'stock', 'category_id']
+        }
+      ],
+    });
+    res.json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
+    // find one category by its `id` value
+    // be sure to include its associated Products
 
-router.post('/', (req, res) => {
-  // create a new category
-});
+    router.post('/', (req, res) => {
+      // create a new category
+      try {
+        const newCategory = await Category.create({
+          category_name: req.body.category_name,
+        })
+        res.json(newCategory);
+      } catch (err) {
+        res.status(500).json(err);
+      }
+      
+    });
 
-router.put('/:id', (req, res) => {
-  // update a category by its `id` value
-});
+    router.put('/:id', (req, res) => {
+      try {
+        const categoryUpdate = await Category.update({
+          where: {
+            category_id = req.params.id,
+          },
+        });
+        res.json(categoryUpdate);
+      } catch (err) {
+        res.status(500).json(err);
+      }
+      
+    });
+      
+      
+      // update a category by its `id` value
+   
 
-router.delete('/:id', (req, res) => {
-  // delete a category by its `id` value
-});
+    router.delete('/:id', (req, res) => {
+      // delete a category by its `id` value
+    });
 
-module.exports = router;
+    module.exports = router;
